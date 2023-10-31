e6488292482e77c9f71298c03e52edc01d9ed06a
Kevins Kosojs <117863532+KeveKeve4@users.noreply.github.com>
2023-11-01 00:37:49 +0200

    Update PU2.c

---
+++ Programmēšanas uzdevumi/PU2/PU2.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+
+int main() 
+{
+unsigned char num1;
+
+printf("Ievadiet naturālu skaitli: ");
+scanf("%hhu", &num1);
+
+printf("Skaitlis %hhu uzrakstīts bināri ir: ", num1 );
+for (int i = 7; i >= 0; i--) 
+	{
+		if ((num1 >> i) & 1)
+		{
+		printf("1");
+		}
+
+		else
+		{
+        printf("0");
+		}
+	}
+
+printf("\n");
+return 0;
+}

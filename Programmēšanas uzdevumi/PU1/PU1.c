c820cf5e20bf74f203226a482e593789582b935e
Kevins Kosojs <117863532+KeveKeve4@users.noreply.github.com>
2023-11-01 00:35:39 +0200

    Add files via upload

---
+++ Programmēšanas uzdevumi/PU1/PU1.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+int main() 
+{
+	int long long num1, num2, sum;
+
+    printf("Ierakstiet 400 000: ");
+    scanf("%lld", &num1);
+    printf("Ieraksiet 100 000: ");
+    scanf("%lld", &num2);
+
+    sum = num1 * num2;
+    
+	if(sum == 40000000000) 
+	{    
+    printf("Iznākums: %lld\n", sum);
+	}
+    else 
+	{ 
+    printf("Ievadītas citas vērtības: %lld\n", sum);
+	}
+    return 0;
+}

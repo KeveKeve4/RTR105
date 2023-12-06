0148fda94d6fb74b5b5b8c9a7c8cf0969de6dd1c
kevekeve4 <kevekeve4@gmail.com>
2023-12-06 10:50:18 +0200

    commit

--- Class_16/cosX2.c
+++ Class_16/cosX2.c
@@ -7,38 +7,21 @@ double mycos (double x)
 
     a = pow(-1,k)*pow(x,4*k)/(1.);
     S = a;
-    printf("%.2f\t%8.2f\t%8.2f\n",x,a,S);
-
+    
     while (k<500)
     {
         k++;
         a = a * (-1) *x*x*x*x / ((2*k)*((2*k)-1));
         S = S + a;
-        printf("%.2f\t%8.2f\t%8.2f\n",x,a,S);
         if(k=1)
-        {
-            printf("a0   = %8.2f S0   = %8.2f\n",a,S);
-        }
+        {printf("a0   = %8.2f S0   = %8.2f\n",a,S);}
         if(k=499)
-        {
-            printf("a499 = %8.2f S499 = %8.2f\n",a,S);
-        }
+        {printf("a499 = %8.2f S499 = %8.2f\n",a,S);}
         if(k=500)
-        {
-            printf("a500 = %8.2f S500 = %8.2f\n",a,S);
-        }
+        {printf("a500 = %8.2f S500 = %8.2f\n",a,S);}
     }
     return S;
 }
-//void main()
-//{
-// double x=1.69,y,yy;
-// y = cos(x*x);
-// printf("standarta funkcija cos - y=cos(%.2f)=%.2f\n",x,y);
-//
-//yy = mycos(x);
-//printf("lictotaaja funkcija - y=mycos(%.2f)=%.2f\n",x,yy);
-//}
 void main()
 {
     double x,y,yy;

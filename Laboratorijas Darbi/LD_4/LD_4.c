3669522cbb36d9089f51fbb940e2b913b6bf06c1
Kevins Kosojs <117863532+KeveKeve4@users.noreply.github.com>
2024-01-11 22:10:41 +0200

    Update LD_4.c

--- Laboratorijas Darbi/LD_4/LD_4.c
+++ Laboratorijas Darbi/LD_4/LD_4.c
@@ -10,76 +10,76 @@ int k, n=2;
 
 int main()
 {
-printf("lūdzu ievadiet sākuma robežu a:\n");
-scanf("%f",&a);
+	printf("lūdzu ievadiet sākuma robežu a:\n");
+	scanf("%f",&a);
   
-printf("lūdzu ievadiet beigu robežu b:\n");
-scanf("%f",&b);
+	printf("lūdzu ievadiet beigu robežu b:\n");
+	scanf("%f",&b);
   
-printf("lūdzu izvēlieties aprēķinu precizitāti:\n");
-scanf("%f",&preciz);
+	printf("lūdzu izvēlieties aprēķinu precizitāti:\n");
+	scanf("%f",&preciz);
 
-printf("Rezultāts izmantojot taisnstūra metodi: %f\n", taisn(integr1));
-printf("Rezultāts izmantojot trapeces metodi: %f\n", trapece(integr1));
-//printf("Rezultāts izmantojot Simpsona metodi: %f\n", simpsons(integr1));
-}
+	printf("Rezultāts izmantojot taisnstūra metodi: %f\n", taisn(integr1));
+	printf("Rezultāts izmantojot trapeces metodi: %f\n", trapece(integr1));
+	//printf("Rezultāts izmantojot Simpsona metodi: %f\n", simpsons(integr1));
+	}
 
-float taisn(float integr1)
-{
-  integr2=(b-a)*pow(cos((a+b)/n),2);
-  while(fabs(integr2-integr1)>preciz)
-  {
-    n*=2;
-    h=(b-a)/n;
-    integr1=integr2;
-    integr2=0;
-    for(k=0;k<n;k++)
-    {
-      integr2+=h*pow(cos(a+(k+0.5)*h),2);
-    }
-  }
-return integr1;
+	float taisn(float integr1)
+	{
+  		integr2=(b-a)*pow(cos((a+b)/n),2);
+  		while(fabs(integr2-integr1)>preciz)
+  		{
+    			n*=2;
+    			h=(b-a)/n;
+    			integr1=integr2;
+    			integr2=0;
+    			for(k=0;k<n;k++)
+    			{
+      			integr2+=h*pow(cos(a+(k+0.5)*h),2);
+    			}
+  		}
+	return integr1;
 }
 
 float trapece(float integr1)
 {
-  integr2=((b-a)/n)*(pow(cos(a),2)*pow(cos(b),2))/n;
-  while(fabs(integr2-integr1)>preciz)
-  {
-    n*=2;
-    h=(b-a)/n;
-    integr1=integr2;
-    integr2=0;
-    for(k=0;k<n;k++)
-    {
-      integr2+=h*pow(cos(a+(k+0.5)*h),2);
-    }
-  }
-return integr1;
+  	integr2=((b-a)/n)*(pow(cos(a),2)*pow(cos(b),2))/n;
+  	while(fabs(integr2-integr1)>preciz)
+  	{
+    		n*=2;
+    		h=(b-a)/n;
+    		integr1=integr2;
+    		integr2=0;
+    		for(k=0;k<n;k++)
+    		{
+      		integr2+=h*pow(cos(a+(k+0.5)*h),2);
+    		}
+  	}
+	return integr1;
 }
 
 /*
 float simpsons(float integr1)
 {
-  h=(b-a)/n;
-  integr1=(h/3)*(pow(cos(a),2)-4*(pow(cos((a+b)/2),2)+pow(cos(b),2)));
-  while(fabs(integr2-integr1)>preciz)
-  {
-    integr1=integr2;
-    integr2=0;
-    for(k=1;k<n;k++)
-    {
-      if(k%2==0)
-      {
-	integr2+=2*pow(cos(a+k*h),2);
-      }
-      else
-      {
-      integr2+=4*pow(cos(a+k*h),2);
-      }
-    }
-  integr1=(h/6)*((pow(cos(a),2)+pow(cos(b),2)+integr2));
-  }
-return integr1;
+  	h=(b-a)/n;
+  	integr1=(h/3)*(pow(cos(a),2)-4*(pow(cos((a+b)/2),2)+pow(cos(b),2)));
+  	while(fabs(integr2-integr1)>preciz)
+  	{
+    		integr1=integr2;
+    		integr2=0;
+    		for(k=1;k<n;k++)
+    		{	
+      			if(k%2==0)
+      			{
+				integr2+=2*pow(cos(a+k*h),2);
+      			}
+      			else
+      			{
+      				integr2+=4*pow(cos(a+k*h),2);
+      			}
+    		}
+  		integr1=(h/6)*((pow(cos(a),2)+pow(cos(b),2)+integr2));
+  	}
+	return integr1;
 }
 */

c17c0d6ad7096838a15b105c429876de64c5d592
Kevins Kosojs <117863532+KeveKeve4@users.noreply.github.com>
2024-01-22 14:28:51 +0200

    Update LD_5.c

--- Laboratorijas Darbi/LD_5/LD_5.c
+++ Laboratorijas Darbi/LD_5/LD_5.c
@@ -4,73 +4,60 @@
 int main()
 { 
 	char input[256]; 
-	int x, y, tmp, length, min=256, max=0, vid_v, medi, vertiba, kop_s=0;
-	
+	int x, y, tmp, length, min=256, max=0, vid_v, vertiba, kop_s=0;
+    
 	printf("Lūdzu ievadiet simbolu rindu (ne vairāk kā 256 rakstzīmēm!):\n"); 
 	scanf("%[^\n]", input);
 
-	length=strlen(input); 
+	length = strlen(input); 
 	printf("Ievadīto simbolu skaits ir %d.\n\n", length);
 
-	for (y=0;y<length;y++)
-	{
-		for (x=0;x<length-1;x++)
+
+    	for (y = 0; y < length; y++)
+    	{
+        	for (x = 0; x < length - 1; x++)
 		{
-			if(input[x]>input[x+1])
-			{
- 				tmp =input[x+1]; input[x+1] = input[x]; input[x]= tmp;
- 			}
- 		}
-	}
-
-	for(x=0;length>x;x++)
-	{
-		vertiba = (int)input[x];
-		if (max<vertiba) max=vertiba;
-		if (min>vertiba && vertiba>32)
-		min = vertiba; kop_s=kop_s+vertiba;
-	}
-
-	printf("Mazākā vērtība ir %d (pēc ASCII), simbols %c;\n\n", min, min);
-	printf("Lielākā vērtība ir %d (pēc ASCII), simbols %c;\n\n", max, max);
-
-	vid_v=kop_s/length; 
+            		if (input[x] > input[x + 1])
+            		{
+                		tmp = input[x + 1]; 
+                		input[x + 1] = input[x]; 
+                		input[x] = tmp;
+            		}
+        	}
+    	}
 	
-	printf("Vidējā vērtība pēc ASCII ir %d;\n\n",vid_v);
-	printf("Ievadītā rindas mediāna ir %d (pēc ASCII), simbols %c;\n\n", input[length/2], input[length/2]);
+	int count[256] = {0};
+	
+    	for (x = 0; x < length; x++)
+    	{
+        	vertiba = (int)input[x];
+        	count[vertiba]++;
+        	kop_s += vertiba;
 
-	for(x=0;length>x;x++)
-	{
- 		if(input[x]==input[x+1])
-		{
- 			y++;
-	 	}
-		else if (input[x] != input[x+1])
-		{
-			if(y>max) max=y;
-			y=0;
-		}
-	}
+        	if (max < vertiba) max = vertiba;
+        	if (min > vertiba && vertiba > 32) min = vertiba;
+    	}
 
-	for(x=0;length>x;x++)
-	{
-		if(input[x]==input[x+1])
-		{
-			y++;
-		}
-		else if(input[x] != input[x+1])
-		{
-			if (max==y)
-			{
-				printf("Rindas moda ir %d, simbols %c; \n\n", input[x],input[x]);
-			}
-			y=0;
-		}
-	}
+    	int maxCount = 0, maxCountIndex;
 
-	for(x=0;length>x;x++)
-	{
-		printf("%d \t %c \t %d \n", x, input[x], input[x] );
-	}
+    	for (x = 0; x < 256; x++)
+    	{
+        	if (count[x] > maxCount)
+        	{
+            		maxCount = count[x];
+            		maxCountIndex = x;
+        	}
+    	}
+
+    	printf("Mazākā vērtība ir %d (pēc ASCII), simbols %c;\n\n", min, min);
+    	printf("Lielākā vērtība ir %d (pēc ASCII), simbols %c;\n\n", max, max);
+    	printf("Vidējā vērtība ir %d (pēc ASCII), simbols %c;\n\n", kop_s / length, kop_s / length);
+    	printf("Ievadītā rindas mediāna ir %d (pēc ASCII), simbols %c;\n\n", input[length / 2], input[length / 2]);
+    	printf("Biežāk sastopamais simbols ir %d (pēc ASCII), simbols %c ar %d sastopamajiem.\n\n", (char)maxCountIndex, (char)maxCountIndex, maxCount);
+
+    	for (x = 0; x < length; x++)
+    	{
+        	printf("%d \t %c \t %d \n", x, input[x], input[x]);
+    	}
 	return 0;
 }
